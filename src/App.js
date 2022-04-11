e875c17750d48b7f98447fce5ce786d936b65982
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-04-11 10:16:46 -0700

    start form and to do list components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header>
+				<h1>Jesse's To Do List</h1>
+			</header>
+      <Form />
+		</div>
+	);
 }
 
 export default App;
