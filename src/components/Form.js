137e0c3345622dec44b2e9ca85144f4d5e0cecc8
Jesse McDermott <mcdermott.jess9@gmail.com>
2022-04-11 12:00:46 -0700

    add submitTodoHandler function to set state of input after onClick event

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,25 @@
-const Form = ({ setInputText }) => {
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
 	const inputTextHandler = (event) => {
 		console.log(event.target.value);
 		setInputText(event.target.value);
 	};
+	const submitToDoHandler = (event) => {
+		event.preventDefault();
+		setTodos([
+			...todos,
+			{ text: inputText, completed: false, id: (Math.random() * 1000).toFixed(2) }
+		]);
+		setInputText(''); //sets state back to nothing
+	};
 	return (
 		<form>
-			<input onChange={inputTextHandler} type="text" className="todo-input" />
-			<button className="todo-button" type="submit">
+			<input 
+				value={inputText} //default value is whatever the setInputText value is on line 12
+				onChange={inputTextHandler}
+				type="text"
+				className="todo-input"
+			/>
+			<button onClick={submitToDoHandler} className="todo-button" type="submit">
 				<i className="fas fa-plus-square" />
 			</button>
 			<div className="select">
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,9 +1,9 @@
-const ToDoList = () => {
-	return (
-		<div className="todo-container">
-			<ul className="todo-list" />
-		</div>
-	);
+const TodoList = () => {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list" />
+        </div>
+    );
 };
 
-export default ToDoList
+export default TodoList
